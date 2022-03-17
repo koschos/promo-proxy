8f4933742af75b9705e8c72bc6aaa318fe7945e9
k.chaus <k.chaus@thebeat.co>
2022-03-17 23:27:19 +0200

    port 80

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 const (
 	sourceURL = "https://intertool.ua/xml_output/yandex_market.xml"
 	frequency = 60 * time.Minute
-	port      = "3000"
+	port      = "80"
 )
 
 var feedStorage storage.FeedStorage
